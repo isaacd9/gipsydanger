40ff55fe83ebbe596d9b29ee3b8812424da1854e
Zack Elliott <hubridnoxx@gmail.com>
2013-08-05 23:39:14 -0700

    [master]  stuff w/ isaac

--- Symfony/web/config.php
+++ Symfony/web/config.php
@@ -4,8 +4,6 @@ if (!isset($_SERVER['HTTP_HOST'])) {
     exit('This script cannot be run from the CLI. Run it from a browser.');
 }
 
-<<<<<<< HEAD
-=======
 if (!in_array(@$_SERVER['REMOTE_ADDR'], array(
     '127.0.0.1',
     '::1',
@@ -14,7 +12,6 @@ if (!in_array(@$_SERVER['REMOTE_ADDR'], array(
     exit('This script is only accessible from localhost.');
 }
 
->>>>>>> 0eb9cd2462beb1c13d1a96454f572d2722b62df4
 require_once dirname(__FILE__).'/../app/SymfonyRequirements.php';
 
 $symfonyRequirements = new SymfonyRequirements();
