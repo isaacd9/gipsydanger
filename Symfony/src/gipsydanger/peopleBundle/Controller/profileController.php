e2f770d0ab2ead8e5fb1be2bccf2145822521908
Isaac Diamond <isaac@rover.com>
2013-08-06 19:15:25 -0700

    [Master] Fixed brokennes around views. Shit should work now. Yay!

--- Symfony/src/gipsydanger/peopleBundle/Controller/profileController.php
+++ Symfony/src/gipsydanger/peopleBundle/Controller/profileController.php
@@ -2,14 +2,11 @@
 
 namespace gipsydanger\peopleBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Response;
 
 class profileController extends Controller
 {
     public function showAction($username)
     {
-          return new Response($username . "'s profileß");
-//        return $this->render('Login page here');
-//        return $this->render('gipsydangerpeopleBundle:profile:profile.html.twig');
+        return $this->render('gipsydangerpeopleBundle:profile:profile.html.twig');
     }
 }
