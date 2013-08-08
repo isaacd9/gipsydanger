8b2493148e31bec5ae57c56c5f14c7cb34d2cf32
Isaac Diamond <isaac@rover.com>
2013-08-07 17:45:19 -0700

    [Master] Continued work on users class.

--- Symfony/src/gipsydanger/peopleBundle/Model/Users.php
+++ Symfony/src/gipsydanger/peopleBundle/Model/Users.php
@@ -3,7 +3,18 @@
 namespace gipsydanger\peopleBundle\Model;
 
 use gipsydanger\peopleBundle\Model\om\BaseUsers;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\AdvancedUserInterface;
 
-class Users extends BaseUsers
+class Users extends BaseUsers implements UserInterface
 {
+	public function getRoles();
+	{
+		$roles=$this->getRoles();
+		foreach($roles as $role)
+		{
+			$returnArray[]=$role->getName();
+		}
+		return $returnArray;
+	}
 }
